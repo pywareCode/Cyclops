1ba3c87d4b16d07ba19d1335f6997962d8f13845
Tyler Spadgenske <tyspadgens@gmail.com>
2014-02-24 17:38:40 -0600

    added more cmds

--- main.py
+++ main.py
@@ -3,6 +3,46 @@
 
 import getcmd
 
-cmd = getcmd.get()
-print cmd
-print cmd.split()
+def main(DEBUG=False):
+    #Main loop
+    while True:
+        #Command getting loop
+        while True:
+            #Get the command and convert it to a list
+            cmd = getcmd.get().split()
+            if DEBUG: print cmd
+            #If first word is "cyclops" Command is for robot so exit command loop
+            if cmd[0].lower() == 'cyclops':
+                break
+
+        cmd.pop(0)
+        num = 0
+        for word in cmd:
+            cmd[num] = word.lower()
+            num += 1
+        print cmd
+
+        master_command = ''
+        #Determin master command
+        if cmd[0] == 'what':
+            master_cmd = 'what'
+        elif cmd[0] == 'walk':
+            master_cmd = 'walk'
+        elif cmd[0] == 'pickup':
+            master_cmd = 'pickup'
+        elif cmd[0] == 'set down':
+            master_cmd = 'set down'
+        elif cmd[0] == 'where':
+            master_cmd = 'where'
+        elif cmd[0] == 'take':
+            master_cmd = 'take'
+        elif cmd[0] == 'set':
+            master_cmd = 'set'
+        elif cmd[0] == 'tell':
+            master_cmd = 'tell'
+        elif cmd[0] == 'who':
+            master_cmd = 'who'
+        print master_command        
+            
+main(DEBUG=True)
+
