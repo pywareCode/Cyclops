ab3abe9ba2f8ae3990ecbe960cf3a07fa211e415
root <root@raspberrypi.(none)>
2014-05-13 20:21:11 +0000

    Edited motor.py

--- motor.py
+++ motor.py
@@ -1,4 +1,5 @@
 #Testing the L298N motor driver
+#Tyler Spadgenske
 
 import time
 import RPi.GPIO as GPIO
@@ -9,11 +10,11 @@ GPIO.setup(12, GPIO.OUT)
 def step(step=1):
     for i in range(0, step):
         GPIO.output(8, True)
-        time.sleep(5)
+        time.sleep(2)
         GPIO.output(8, False)
         
         #GPIO.output(12, True)
-        #time.sleep(2)
+        #time.sleep(1)
         #GPIO.output(12, False)
         
     GPIO.output(8, False)
