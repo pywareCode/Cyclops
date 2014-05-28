42a82856e9f64d163e9f8cf13e6410b0a626baed
Tyler Spadgenske <tyspadgens@gmail.com>
2014-05-28 08:50:19 -0500

    Changed Client program

--- src/client.py
+++ src/client.py
@@ -1,22 +1,52 @@
-#Cyclops CLI Client
+
+#Andy CLI Client
 #By Tyler Spadgenske
 
-import socket
+import socket, sys, time
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '192.168.0.104'
-port = 5150
+class Client():
+    def __init__(self):
+        self.logo()
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = '192.168.0.104'
+        self.port = 5150
+        try:
+            self.server.connect((self.host, self.port))
+            self.data = self.server.recv(1024)
+            print bytes.decode(self.data)
+        except:
+            print 'Cannot Connect to Andy'
+            time.sleep(10)
+            sys.exit()
 
-server.connect((host, port))
-data = server.recv(1024)
-print bytes.decode(data)
+        
+    def logo(self):
+        print'           _   _ _______     __'
+        print'     /\   | \ | |  __ \ \   / /'
+        print'    /  \  |  \| | |  | \ \_/ / '
+        print'   / /\ \ | . ` | |  | |\   /  '
+        print'  / ____ \| |\  | |__| | | |   '
+        print' /_/    \_\_| \_|_____/  |_|'
+        print'+++++++++++++++++++++++++++++++++++++++++++++++'                             
+        print
+        print 'ANDY Humanoid Robot CLI Interface'
+        print 'Copyright (c) 2014 Tyler Spadgenske'
+        print
+        print '+++++++++++++++++++++++++++++++++++++++++++++++'
+        print 'Type "cmds" for list of cmds, "help" for help, "exit" for exiting.'
 
-while True:
-    data = raw_input('>>')
-    server.send(str.encode(data))
-    if data == 'exit':
-        break
+    def send_cmd(self):
 
+        while True:
+            self.data = raw_input('>>')
+            self.server.send(str.encode(self.data))
+            if self.data == 'exit':
+                break
 
-print 'Closing Connection...'
-server.close()
+    def end(self):
+        print 'Closing Connection...'
+        self.server.close()
+
+if __name__ == '__main__':
+    cli = Client()
+    cli.send_cmd()
