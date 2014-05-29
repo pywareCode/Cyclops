3b0b21b9b3b1fab6b0fb023f53d78b75f69735cb
root <tyspadgens@gmail.com>
2014-05-29 20:12:00 +0000

    Finished Server

--- src/getcmd.py
+++ src/getcmd.py
@@ -7,14 +7,17 @@ class Get_cmd():
         self.microphone = mic.Mic(lmd='models/lm.lm', dictd='models/dic.dic',
                      lmd_persona='models/waitlm.lm', dictd_persona='models/waitdic.dic')
         self.run_client = False
+        self.client = None
         
     def get(self):
         while True:
             while True:
+                self.run_client = False
                 self.client_cmd = self.get_client_cmd()
                 if self.client_cmd != None:
                     self.cmd = self.client_cmd
                     self.run_client = True
+                    os.system('sudo rm cmd.txt')
                     break
                 
                 self.word = self.microphone.passiveListen('ANDY')
@@ -23,7 +26,6 @@ class Get_cmd():
 
             if self.run_client != True:
                 self.cmd = self.microphone.activeListen()
-                self.run_client = False
                 
             print 'COMMAND: ', self.cmd
             return self.cmd
@@ -34,15 +36,11 @@ class Get_cmd():
             self.readit = open('/home/pi/ANDY/src/cmd.txt', 'r')
             self.lines = self.readit.readlines()
             if self.lines != []:
-                print self.lines[0]
                 self.client = self.lines[0]
-
-            os.remove('/home/pi/ANDY/src/cmd.txt')
             self.readit.close()
 
-            self.client = None
-
         return self.client
+    
 def get_age():
     microphone = mic.Mic(lmd='models/agelm.lm', dictd='models/agedic.dic',
                      lmd_persona='models/waitlm.lm', dictd_persona='models/waitdic.dic')
--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
-#Cyclops server test
+#Andy server test
 #By Tyler Spadgenske
 
-import socket, os, time
+import socket, os, time, sys
 from tts import say
 
 class Server():
@@ -17,32 +17,36 @@ class Server():
         self.client.send(str.encode('Connection made with Andy'))
 
     def get_cmd(self):
-        self.data = self.client.recv(1024)
+        try:
+            self.data = self.client.recv(1024)
+        except:
+            print 'Lost Connection with ', self.addr
+            return None
         print 'Recieved data from client: ', bytes.decode(self.data)
 
-        return self.data
+        return bytes.decode(self.data)
 
     def end(self):
         print 'Closing connection...'
-        self.client.send(str.encode('exit'))
         self.client.close()
         
 def start():
     serv = Server()
     while True:
         files = open('/home/pi/ANDY/src/cmd.txt', 'w+')
+        os.system('sudo chmod 777 /home/pi/ANDY/src/cmd.txt')
         cmd = serv.get_cmd()
+        if cmd == None:
+            break
         files.write(cmd)
         files.close()
         while True:
             if os.path.isfile('/home/pi/ANDY/src/cmd.txt') == False:
                 break
         
-    serv.end()
-    print 'IT QUIT!'
-    time.sleep(6)
-        
+    serv.end()      
 
 if __name__ == '__main__':
-    start()
+    while True:
+        start()
             
